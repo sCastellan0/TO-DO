fb9c5e1c960f7e8922459ff5d4898677d8c4bbd7
sCastellan0 <castellanoponssergio@gmail.com>
2026-06-14 14:02:09 +0200

    Primer commit

---
+++ Proyecto TO-DO/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace Proyecto_TO_DO
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Tarea> Tareas { get; set; }
+        public Tarea NuevaTarea { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Tareas = new ObservableCollection<Tarea>();
+            NuevaTarea = new Tarea
+            {
+                Nombre = "",
+                FechaLimite = DateTime.Today.AddDays(1),
+                Prioridad = Prioridad.Media,
+                Completada = false
+            };
+
+            DataContext = this;
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NuevaTarea.Nombre) &&
+                NuevaTarea.FechaLimite > DateTime.Today)
+            {
+                var tarea = new Tarea
+                {
+                    Nombre = NuevaTarea.Nombre,
+                    FechaLimite = NuevaTarea.FechaLimite,
+                    Prioridad = NuevaTarea.Prioridad,
+                    Completada = false
+                };
+
+                Tareas.Add(tarea);
+
+                // Limpiar formulario
+                NuevaTarea.Nombre = "";
+                NuevaTarea.FechaLimite = DateTime.Today.AddDays(1);
+                NuevaTarea.Prioridad = Prioridad.Media;
+            }
+            else
+            {
+                MessageBox.Show("Nombre o fecha no válida",
+                                "Validación",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Warning);
+            }
+        }
+
+        private void BtnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            var completadas = Tareas.Where(t => t.Completada).ToList();
+            foreach (var tarea in completadas)
+            {
+                Tareas.Remove(tarea);
+            }
+        }
+    }
+}
---
+++ Proyecto TO-DO/Tarea.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Proyecto_TO_DO
+{
+    public class Tarea : INotifyPropertyChanged
+    {
+        private string _nombre;
+        private DateTime _fechaLimite;
+        private Prioridad _prioridad;
+        private bool _completada;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set { _nombre = value; OnPropertyChanged(); }
+        }
+
+        public DateTime FechaLimite
+        {
+            get => _fechaLimite;
+            set { _fechaLimite = value; OnPropertyChanged(); }
+        }
+
+        public Prioridad Prioridad
+        {
+            get => _prioridad;
+            set { _prioridad = value; OnPropertyChanged(); }
+        }
+
+        public bool Completada
+        {
+            get => _completada;
+            set { _completada = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public enum Prioridad
+    {
+        Baja,
+        Media,
+        Alta
+    }
+}
